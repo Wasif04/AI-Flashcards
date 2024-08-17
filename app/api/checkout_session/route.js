a6748d30c59c91e124d7932b0b05f5dbf5e23694
requisz <prk3479@gmail.com>
2024-08-17 15:22:39 -0400

    Finished video code.

--- app/api/checkout_session/route.js
+++ app/api/checkout_session/route.js
@@ -1,13 +1,27 @@
 import { NextResponse } from 'next/server'
 import Stripe from 'stripe'
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY),
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
-const formatAmountForStripe = (amount, currency) => {
+const formatAmountForStripe = (amount) => {
     return Math.round(amount * 100)
-   }
+}
 
+
+export async function GET(req){
+
+  const searchParams = req.nextUrl.searchParams
+  const session_id = searchParams.get('session_id')
+
+  try{
+    const checkoutSession = await stripe.checkout.sessions.retrieve(session_id)
+    return NextResponse.json(checkoutSession)
+  }
+  catch (error){
+    console.error('Error retrievin checkout session:', error)
+    return NextResponse.json({error: {message: error.message}}, {status: 500})
+  }
+}
 export async function POST(req) {
-  try {
     const params = {
         mode: 'subscription',
         payment_method_types: ['card'],
@@ -40,10 +54,4 @@ export async function POST(req) {
       return NextResponse.json(checkoutSession, {
         status: 200,
       })
-  } catch (error) {
-    console.error('Error creating checkout session:', error)
-    return new NextResponse(JSON.stringify({ error: { message: error.message } }), {
-      status: 500,
-    })
-  }
 }
